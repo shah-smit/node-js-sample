48ecf9c6832dd4acb9aa0bd64a2ab2d6c0743b4b
Smit Shah <raja95shah@gmail.com>
2018-06-09 20:08:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -7,6 +8,16 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
+
+app.get("/custommessagee/:message", function (request, response) {
+  var message = request.params.message;
+  message = message.split('_').join(' ');
+  var vresponse = new VoiceResponse();
+  vresponse.say(message);
+  response.set('Content-Type', 'text/xml');
+  response.type('text/xml');
+  response.send(vresponse.toString());
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
